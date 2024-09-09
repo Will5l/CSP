fb8a058c4f4b665e6ccdee06fffc49de22af1eed
Will5l <Will.malloch@ucas-edu.net>
2024-09-09 20:19:43 +0000

    C done.

--- C/firstProgram.c
+++ C/firstProgram.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main(void){
-    char name[] = ;
-
-
-
+    char color[30];
+    printf("What's your favorite color?: \n");
+    scanf("%s", color);
+    printf("%s is a nice color", color);
 
 
 
