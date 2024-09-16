bc0317eefd428ce1ff3a7d36db4a27c93d07e957
Will5l <Will.malloch@ucas-edu.net>
2024-09-16 20:42:07 +0000

    python calc

---
+++ Python/financialCalculator.py
@@ -0,0 +1,9 @@
+print("Welcome to your financial calculator\n")
+income = input("What's your monthly income?: ")
+rent = input("What's your monthly rent?: ")
+utilities = input("What's your monthly utilities?: ")
+groceries = input("What's your monthly groceries?: ")
+Transport = input("What's your monthly transportation costs?: ")
+print("Your monthly income is ${}")
+
+
