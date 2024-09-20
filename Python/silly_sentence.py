44a43b74036def9693d67e782880449c0fa6ceb4
Will5l <Will.malloch@ucas-edu.net>
2024-09-20 20:20:43 +0000

    python

---
+++ Python/silly_sentence.py
@@ -0,0 +1,9 @@
+name = input("Type a name: ")
+    
+place = input("Type a place: ")
+  
+verb = input("Type a verb: ")
+  
+noun = input("Type a noun: ")
+sillySentence = name + " went to the " + place + " where they " + verb + " and bought a " + noun
+print(sillySentence)
