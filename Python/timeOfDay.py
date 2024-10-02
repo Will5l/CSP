dc4b843fcbf8a341b921771d01f1adb41ba3a586
Will5l <Will.malloch@ucas-edu.net>
2024-10-02 20:28:04 +0000

    time of days

--- Python/timeOfDay.py
+++ Python/timeOfDay.py
@@ -1,10 +1,15 @@
-Time = int(input("What time is it? (In 24hr): "))
-if (Time >= 1 and Time <=11):
+import datetime
+
+now = datetime.datetime.now().hour
+
+print(now)
+
+if (now <=11):
     print("Good Morning")
-elif (Time >= 12 and Time <=16):
+elif (now <=16):
     print("Good afternoon")
-elif (Time >= 17 and Time <=24):
+elif (now <=20):
     print("Good Evening")
 else:
-    print("Invalid")
+    print("GOOD NIGHT. GO TO BED.")
 
