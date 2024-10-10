602987621ab9792fea66cccd0d950732ac741c09
Will5l <Will.malloch@ucas-edu.net>
2024-10-10 20:42:05 +0000

    funni

--- C/fizzBuzz.c
+++ C/fizzBuzz.c
@@ -1,18 +1,38 @@
 #include <stdio.h>
+#include <string.h>
+#include <time.h>
+int xx, user;
+char one[50], two[50], three[50];
 
-int xx;
+void delay(int secs){
+    int milli = 1000*secs;
+
+    clock_t start = clock();
+
+    while(clock()<start+milli);
+}
 
 int main(void){
- for(xx=1;xx<=50;xx++){
+
+printf("Give number: \n");
+scanf("%d", &user);
+printf("Give word: \n");
+scanf("%s", &one);
+printf("Give word: \n");
+scanf("%s", &two);
+strcat(three, one);
+strcat(three, two);
+ for(xx=1;xx<=user;xx++){
         if (xx%3 == 0 && xx%5 == 0){
-            printf("FizzBuzz\n");
+            printf("%s\n", three);
         }else if (xx%3 == 0){
-            printf("Fizz\n");
+            printf("%s\n", one);
         }else if (xx%5 == 0){
-            printf("Buzz\n");
+            printf("%s\n", two);
         }else{
             printf("%d\n", xx);
         }
+        delay(0);
  }
 return 0;
 }
